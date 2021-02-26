4deb6c6357c08e78183ccd355546bda9497ee9f0
var-kyle <kyle.meelker@gmail.com>
2021-02-26 13:00:15 -0500

    setting up jest and adding a unit test for date

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testPathIgnorePatterns: ['/node_modules', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+};
