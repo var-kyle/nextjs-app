97ab584a2f85e04afccc5a99b9a3149cf6a6e3da
var-kyle <kyle.meelker@gmail.com>
2021-03-01 09:52:10 -0500

    setting up initial geocoding

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,9 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import Search from '../components/search';
 import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
-import Date from '../components/date';
+import { useRouter } from 'next/router';
 
-export default function Home({ allPostsData }) {
+export default function Home({ lat, long }) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +13,20 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <Search />
       </section>
+      <section>
+        Lat: {lat}
+        Long: {long}
+      </section>
     </Layout>
   );
 }
+
+Home.getInitialProps = async (ctx) => {
+  const search = ctx.query.q;
+  console.log(ctx.query);
+
+  return {
+    lat: 100.22,
+    long: -45.23,
+  };
+};
