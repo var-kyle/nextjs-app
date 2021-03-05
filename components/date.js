f57b73bac8fcb2756cef28de284f6227697677a7
var-kyle <kyle.meelker@gmail.com>
2021-03-05 12:46:41 -0500

    fixed unit test

--- components/date.js
+++ components/date.js
@@ -1,14 +1,12 @@
-import { parseISO, format } from 'date-fns';
 import styles from './date.module.css';
 
 export default function Date({ dateString }) {
-  const date = parseISO(dateString);
   return (
     <>
       <input
         type="date"
         name="date"
-        defaultValue={format(date, 'yyyy-mm-dd')}
+        defaultValue={dateString}
         title="Search date"
         className={`${styles.dateInput}`}
       />
